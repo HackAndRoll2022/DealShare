1defc516463a476de9dd2bd08206376467c44c9c
marcustw <e0014884@u.nus.edu.sg>
2022-01-08 01:44:38 +0800

    feat: add cors middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,9 +3,22 @@ from pymongo import MongoClient
 from .auth import AuthHandler
 from .schemas import RegisterDetails, LoginDetails, User
 from .config import mongodb_uri
-import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient(mongodb_uri, connect=False)
 
